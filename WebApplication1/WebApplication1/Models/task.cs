241ca018642b1279e14179502c3e3bf352788b85
tshavor <thomasshavor@gmail.com>
2017-01-16 20:05:49 -0600

    model constructed

---
+++ WebApplication1/WebApplication1/Models/task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class task
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public Status Taskstatus { get; set; }
+
+        public DateTime CompletedOn { get; set; }
+
+    }
+
+    public enum Status
+    {
+        ToDo, InProgress, Complete
+    }
+}
+
+
